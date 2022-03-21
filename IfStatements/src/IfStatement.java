b0d9a3e4cefe3b89f535f755d049a94919c7eb81
Anuj.Upadhyay <anuj.upadhyay@rsystems.com>
2022-03-21 23:40:42 +0530

    DEV-5 - added if statements and started switches.

---
+++ IfStatements/src/IfStatement.java
@@ -0,0 +1,15 @@
+public class IfStatement {
+	public static void main(String[] args) {
+		int age = 14;
+
+		if (age >= 75) {
+			System.out.println("Pranam dadaji!");
+		} else if (age >= 13) {
+			System.out.println("You're cool but still no allowed");	
+		} else if (age >= 18) {
+			System.out.println("You are eligble!");	
+		} else {
+			System.out.println("You are not allowed!");
+		}
+	}
+}
