4738df41abd55b68e67b463a1780ce0f844f166c
Anuj.Upadhyay <anuj.upadhyay@rsystems.com>
2022-03-22 15:03:29 +0530

    DEV-5 - Conditionals: if, switches and logical operators done.

--- Switches/src/Switches.java
+++ Switches/src/Switches.java
@@ -1,6 +1,8 @@
+import java.util.*;
+
 public class Switches {
     public static void main(String[] args) {
-			String day = "Friday";
+			String day = "Tuesday";
 
 			switch(day) {
 				case "Sunday": System.out.println("It is Sunday!");	
@@ -17,6 +19,28 @@ public class Switches {
 				break;
 				case "Saturday": System.out.println("It is Saturday!");	
 				break;
+				default: System.out.println("That is not a day!");
+			}
+
+			// Logical Operators
+			int temp = 25;
+
+			if (temp > 30) {
+					System.out.println("It is hot outside.");
+			} else if (temp >= 20 && temp <= 30) {
+					System.out.println("It is warm outside.");
+			} else {
+					System.out.println("It is cold outside.");
+			}
+
+			Scanner sc = new Scanner(System.in);
+			System.out.println("You are playing a game! Press q or Q to quit");
+			String response = sc.next();
+
+			if (!response.equals("q") && !response.equals("Q")) {
+				System.out.println("You are still playing the game *Pew Pew*");
+			} else {
+				System.out.println("You quit the game!");
 			}
     }
 }
