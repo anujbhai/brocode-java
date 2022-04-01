ce6ca4f5d86967125ea6a01b6bf339be7b7c1d6d
Anuj.Upadhyay <anuj.upadhyay@rsystems.com>
2022-04-01 23:53:17 +0530

    DEV-9 - started variable scope.

--- Objects/src/Main.java
+++ Objects/src/Main.java
@@ -1,13 +1,5 @@
 public class Main {
 	public static void main(String[] args) {
-		Car myCar1 = new Car();
-		Car myCar2 = new Car();
-
-		System.out.println(myCar1.make + " " + myCar1.model);
-		System.out.println(myCar2.make + " " + myCar2.model);
-
-		myCar1.drive();
-		myCar1.brake();
+		DIceRoller dice = new DIceRoller();
 	}
-
 }
