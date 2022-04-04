92f9e10ad872110d12f7656ebf8cb05e876eb49a
Anuj.Upadhyay <anuj.upadhyay@rsystems.com>
2022-04-04 16:58:47 +0530

    DEV-9 - Finished object toString method and object arrays.

--- Objects/src/DIceRoller.java
+++ Objects/src/DIceRoller.java
@@ -1,7 +1,16 @@
-public class DIceRoller {
-	DIceRoller() {}
+import java.util.*;
 
-	void roll() {
-	
+public class DIceRoller {
+	Random random;
+	int n;
+
+	DIceRoller() {
+		random = new Random();
+		roll(random, n);
+	}
+
+	void roll(Random random, int n) {
+		n = random.nextInt(9) + 1;	
+		System.out.println(n);
 	}
 }
--- Objects/src/Main.java
+++ Objects/src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
 	public static void main(String[] args) {
-		DIceRoller dice = new DIceRoller();
+		// Food[] fridge = new Food[3];
+		Food food1 = new Food("pizza");
+		Food food2 = new Food("burger");
+		Food food3 = new Food("hotdog");
+
+		Food[] fridge = {food1, food2, food3};
+
+		// fridge[0] = food1;
+		// fridge[1] = food2;
+		// fridge[2] = food3;
+
+		System.out.println(fridge[0].name);
+		System.out.println(fridge[1].name);
+		System.out.println(fridge[2].name);
 	}
 }
