80a35d6f505065afe1ffcd58120f84cef4c0c12f
Anuj.Upadhyay <anuj.upadhyay@rsystems.com>
2022-04-16 22:42:34 +0530

    DEV-10 - OOP: finished access modifiers.

---
+++ AccessModifiers/src/package1/A.java
@@ -0,0 +1,9 @@
+package package1;
+
+public class A {
+    protected String protectedMessage = "This is protected.";
+    public static void main(String[] args) {
+        B b = new B();
+        System.out.println();
+    }
+}
