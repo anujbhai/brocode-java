fc6889e5df8f03fb34d10b7bed746684274b0b94
Anuj.Upadhyay <anuj.upadhyay@rsystems.com>
2022-05-14 23:42:51 +0530

    DEV-12 - working on labels.

--- GUI/src/Labels.java
+++ GUI/src/Labels.java
@@ -4,10 +4,19 @@ public class Labels {
     // JLabel = a GUI display area for a string of text, an image, or both
 
     public static void main(String[] args) {
+        ImageIcon image = new ImageIcon("boy.png");
+
+        JLabel label = new JLabel();
+        label.setText("This is the painkiller!");
+        label.setIcon(image);
+        label.setHorizontalTextPosition(JLabel.CENTER);
+        label.setVerticalTextPosition(JLabel.TOP);
+
         JFrame labelFrame = new JFrame();
         labelFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        labelFrame.setSize(500, 500);
+        labelFrame.setSize(800, 600);
         labelFrame.setVisible(true);
+        labelFrame.add(label);
     }
 
 }
